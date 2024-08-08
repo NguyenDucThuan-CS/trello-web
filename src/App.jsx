214ca0bfed37677bea6e0276310d743f9c8a6e27
Thuan Nguyen <nguyenducthuan8362@gmail.com>
2024-08-08 20:15:29 +0700

    custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const { mode, setMode } = useColorScheme()
+  console.log('modezzzz', mode);
 
   return (
     <>
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
     </>
   )
 }
